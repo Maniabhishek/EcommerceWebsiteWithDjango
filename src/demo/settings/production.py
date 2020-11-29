01f2fdfe6d1854b318bdb1abe5a575e6b93bc0f7
abhishek mani <maniabhishek05@gmail.com>
2020-11-29 05:56:29 +0530

    another push

--- src/demo/settings/production.py
+++ src/demo/settings/production.py
@@ -1,16 +1,16 @@
 from .base import *
 
-DEBUG = False
+DEBUG = config('DEBUG' , cast=bool)
 
 ALLOWED_HOSTS = ['IP-ADDRESS','WWW.MY-WEBSITE.COM']
 
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME':'your database',
-        'USER':'your db user name',
-        'PASSWORD':'YOUR DB PASSWORD',
-        'HOST': 'LOCALHOST',
+        'NAME':config('DB_NAME'),
+        'USER':config('DB_USER'),
+        'PASSWORD':config('DB_PASSWORD'),
+        'HOST': config('DB_HOST'),
         'PORT':'',
     }
 }
